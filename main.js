dc4e61da035276636166d01ca75f9851db40780f
Klaus Förster <klaus.foerster@uibk.ac.at>
2022-06-15 12:58:48 +0200

    use https:// for eGrundkarte to avoid mixed content

--- main.js
+++ main.js
@@ -9,22 +9,22 @@ let innsbruck = {
 // WMTS Hintergrundlayer der eGrundkarte Tirol definieren
 const eGrundkarteTirol = {
     sommer: L.tileLayer(
-        "http://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png", {
+        "https://wmts.kartetirol.at/gdi_summer/{z}/{x}/{y}.png", {
             attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
         }
     ),
     winter: L.tileLayer(
-        "http://wmts.kartetirol.at/gdi_winter/{z}/{x}/{y}.png", {
+        "https://wmts.kartetirol.at/gdi_winter/{z}/{x}/{y}.png", {
             attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
         }
     ),
     ortho: L.tileLayer(
-        "http://wmts.kartetirol.at/gdi_ortho/{z}/{x}/{y}.png", {
+        "https://wmts.kartetirol.at/gdi_ortho/{z}/{x}/{y}.png", {
             attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`
         }
     ),
     nomenklatur: L.tileLayer(
-        "http://wmts.kartetirol.at/gdi_nomenklatur/{z}/{x}/{y}.png", {
+        "https://wmts.kartetirol.at/gdi_nomenklatur/{z}/{x}/{y}.png", {
             attribution: `Datenquelle: <a href="https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol">eGrundkarte Tirol</a>`,
             pane: "overlayPane",
         }
